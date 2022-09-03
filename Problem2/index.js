945eea5e3b9677b3095d64375fe9d5863c4eb418
Paramkushamlokesh <paramkushamlokesh@gmail.com>
2022-09-03 17:52:19 +0530

    second uploaded

---
+++ Problem2/index.js
@@ -0,0 +1,6 @@
+var name = "Paramkusham lokesh";
+console.log(name);
+var name = "Paramkusham ravinder";
+console.log(name);
+var name = "Paramkusham ramalakshmi";
+console.log(name);
---
+++ problem4/index.js
@@ -0,0 +1,28 @@
+const name = "Paramkusham Lokesh";
+var school = "Masai high school",grade = "A+",section = "c",rollno = "fw22_0919";
+const subject1_marks = 99,subject2_marks = 100,subject3_marks = 98;
+var sum = (subject1_marks +subject2_marks+ subject3_marks);
+
+
+console.log("          𝓇ℯ𝓅ℴ𝓇𝓉 𝒸𝒶𝓇𝒹       ");
+
+
+
+console.log("**********************************");
+console.log("School          :"+school);
+console.log("student name    :"+name);
+console.log("student Rollno  :"+rollno);
+console.log("student section :"+section);
+console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+console.log("<subjects>      score");
+console.log("subject1        "+subject1_marks);
+console.log("subject2        "+subject2_marks);
+console.log("subject3        "+subject3_marks);
+console.log("---------------------------------");
+console.log("         Total ="+sum);
+console.log("         Grade :"+grade);
+console.log("---------------------------------");
+console.log("              TQ          ");
+
+
+
